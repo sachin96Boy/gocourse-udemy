9bfbb03061a61cef68728433a5c289eed7ba6936
sachin supunthaka <sachinsupunthaka96@gmail.com>
2025-02-25 23:13:42 +0530

    init: bank

--- bank/bank.go
+++ bank/bank.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	fmt.Println("Welcome to Go Bank")
+	fmt.Println("What do you want to do")
+	fmt.Println("1. Check Balance")
+	fmt.Println("2. Deposit Money")
+	fmt.Println("3. Withdraw Money")
+	fmt.Println("4. Exit")
+
+	var choice int
+	fmt.Print("your choice: ")
+	fmt.Scan(&choice)
+
+	fmt.Println("your choice: ", choice)
 }
